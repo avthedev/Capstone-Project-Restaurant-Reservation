de7f43534d417035b436f5f0bdc48d74e421f93a
Anon Vanichyanukroh <anonvanichyanukroh@gmail.com>
2021-12-01 16:50:39 -0800

    deleted old stuff

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
 
+//needed to pull correct URL from backend
 import dotenv from "dotenv";
 dotenv.config();
 
