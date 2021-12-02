b47850390377823a8b2fd30700e3776c6d640e81
Anon Vanichyanukroh <anonvanichyanukroh@gmail.com>
2021-12-01 16:47:10 -0800

    deleted old files

--- front-end/src/(dontusethisanymore) components/Backdrop/Backdrop.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Backdrop.css";
-
-function Backdrop({ backdropClickHandler }) {
-  return <div className="backdrop" onClick={backdropClickHandler} />;
-}
-
-export default Backdrop;
